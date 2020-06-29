32e70865d24d3797890241506ad4cbb44213cb1d
Nhat Minh <nhatngo11a1@gmail.com>
2020-06-29 21:33:44 +0700

    Updates

---
+++ precache-manifest.039e948cf586e236da7355996306b823.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e9d905a4beeb888e83c1b8a962d481",
+    "url": "/counting/index.html"
+  },
+  {
+    "revision": "e069eae6345120c626b1",
+    "url": "/counting/static/css/main.bf85fe7c.chunk.css"
+  },
+  {
+    "revision": "4593a87e48932e5a769f",
+    "url": "/counting/static/js/2.e79311ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/counting/static/js/2.e79311ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e069eae6345120c626b1",
+    "url": "/counting/static/js/main.fcab0ddc.chunk.js"
+  },
+  {
+    "revision": "d14b081088d14681ada9",
+    "url": "/counting/static/js/runtime-main.4c68544a.js"
+  },
+  {
+    "revision": "96037b82cd18cfb7b4ce188d97d8a771",
+    "url": "/counting/static/media/apple.96037b82.svg"
+  },
+  {
+    "revision": "5d38528cf2955177d7a1e4a45b46ee98",
+    "url": "/counting/static/media/aubergine.5d38528c.svg"
+  },
+  {
+    "revision": "212bec087c5eb8ed1f98c7f8150ac4d3",
+    "url": "/counting/static/media/avocado.212bec08.svg"
+  },
+  {
+    "revision": "71e1a70cb758b120009657b3841d87e5",
+    "url": "/counting/static/media/ballon.71e1a70c.svg"
+  },
+  {
+    "revision": "b6f14cc52d3c98e60259a8122fc25580",
+    "url": "/counting/static/media/balloon.b6f14cc5.png"
+  },
+  {
+    "revision": "60460b3f3ad4474da60b8b9bd7c8539f",
+    "url": "/counting/static/media/balloons.60460b3f.svg"
+  },
+  {
+    "revision": "fff0a950505b7ad8983b71fba36ead18",
+    "url": "/counting/static/media/banana.fff0a950.svg"
+  },
+  {
+    "revision": "9a38de60b442711e9fd85bc51360fc98",
+    "url": "/counting/static/media/bananas.9a38de60.svg"
+  },
+  {
+    "revision": "e51e28c04f99b168c85e7ffec8800ed0",
+    "url": "/counting/static/media/bear.e51e28c0.svg"
+  },
+  {
+    "revision": "5c95bfbc0815bd3f514eb36227c242f3",
+    "url": "/counting/static/media/bg.5c95bfbc.png"
+  },
+  {
+    "revision": "c84a1df771e5b2eacd0fb2977cf90fa8",
+    "url": "/counting/static/media/bg.c84a1df7.svg"
+  },
+  {
+    "revision": "33d1800950dbc2d547c8d80278b7ea8d",
+    "url": "/counting/static/media/bird1.33d18009.png"
+  },
+  {
+    "revision": "07e7e3fcfb167dc843484e3fe2681593",
+    "url": "/counting/static/media/bird2.07e7e3fc.png"
+  },
+  {
+    "revision": "21d8c744cd9dec1b044c326b2b8d133c",
+    "url": "/counting/static/media/blueberry.21d8c744.svg"
+  },
+  {
+    "revision": "75a8a496d7fe05bfb853ba894ff33b51",
+    "url": "/counting/static/media/broccoli.75a8a496.svg"
+  },
+  {
+    "revision": "1c32752e714ca9caefd5f31b2d824e09",
+    "url": "/counting/static/media/bush.1c32752e.svg"
+  },
+  {
+    "revision": "ae4da5aee02409aac649e27927cb3cd5",
+    "url": "/counting/static/media/cabbage.ae4da5ae.svg"
+  },
+  {
+    "revision": "89cafa12ea2a4bbdae27d306bbf150d8",
+    "url": "/counting/static/media/cake.89cafa12.svg"
+  },
+  {
+    "revision": "d77c0766224cc6c861bada4168cd94b4",
+    "url": "/counting/static/media/car.d77c0766.png"
+  },
+  {
+    "revision": "7dfd44f205d28ec0f79c7bf4924f54d8",
+    "url": "/counting/static/media/carrot.7dfd44f2.svg"
+  },
+  {
+    "revision": "17206da723904cf912003e5eb253ef66",
+    "url": "/counting/static/media/croissant.17206da7.svg"
+  },
+  {
+    "revision": "ff80be775685c5aae48aba7bae79dca5",
+    "url": "/counting/static/media/dino3.ff80be77.svg"
+  },
+  {
+    "revision": "30734eac185216390716ad43cca31cdb",
+    "url": "/counting/static/media/done.30734eac.svg"
+  },
+  {
+    "revision": "705c973bc0a54cde4248c321bff96bd0",
+    "url": "/counting/static/media/eggs.705c973b.svg"
+  },
+  {
+    "revision": "e4cab1df58c957502ba1fdabb0bc528c",
+    "url": "/counting/static/media/filter.e4cab1df.svg"
+  },
+  {
+    "revision": "d42c488b5c731943767f71d365314dc8",
+    "url": "/counting/static/media/flower1.d42c488b.svg"
+  },
+  {
+    "revision": "11be6a407f71610d05cf35e895aab424",
+    "url": "/counting/static/media/grapes.11be6a40.svg"
+  },
+  {
+    "revision": "e87899aec4a72f04c2466f49e6d40f8f",
+    "url": "/counting/static/media/heart.e87899ae.svg"
+  },
+  {
+    "revision": "ed55622d3cca870f9291faabb381e1d9",
+    "url": "/counting/static/media/house.ed55622d.svg"
+  },
+  {
+    "revision": "39b28772c3fd086a2f5a7027dd61ce66",
+    "url": "/counting/static/media/house4.39b28772.png"
+  },
+  {
+    "revision": "84db2cf4905748590a433e8fddf556d9",
+    "url": "/counting/static/media/jam.84db2cf4.svg"
+  },
+  {
+    "revision": "6e1472eb3e0c2a0d14780264019af2d8",
+    "url": "/counting/static/media/kiwi.6e1472eb.svg"
+  },
+  {
+    "revision": "52e9a92507ecad87368fb703686eb5ff",
+    "url": "/counting/static/media/leek.52e9a925.svg"
+  },
+  {
+    "revision": "0a05f035f06fcae96168fc9bd91d3558",
+    "url": "/counting/static/media/lemon.0a05f035.svg"
+  },
+  {
+    "revision": "ba1eeafc0ee56cb900b8bec659734eec",
+    "url": "/counting/static/media/mango.ba1eeafc.svg"
+  },
+  {
+    "revision": "16426bdb5f7bc5776a6df8ff63b2dff4",
+    "url": "/counting/static/media/milk.16426bdb.svg"
+  },
+  {
+    "revision": "fcaed136723f71aa611deee74f159e67",
+    "url": "/counting/static/media/mushroom.fcaed136.svg"
+  },
+  {
+    "revision": "9c594fbbc2954a402815223d23161b12",
+    "url": "/counting/static/media/mustard.9c594fbb.svg"
+  },
+  {
+    "revision": "fb4880daa21ea087400ee69e783f8e36",
+    "url": "/counting/static/media/onion.fb4880da.svg"
+  },
+  {
+    "revision": "5a7f032e33fbff4ce480a0e086ed6fba",
+    "url": "/counting/static/media/orange.5a7f032e.svg"
+  },
+  {
+    "revision": "b7c510737e2c2a186fb5bbb45efd4df0",
+    "url": "/counting/static/media/pasta.b7c51073.svg"
+  },
+  {
+    "revision": "ce0b05ef979f7be2bc953f2a47c6766f",
+    "url": "/counting/static/media/pineapple.ce0b05ef.svg"
+  },
+  {
+    "revision": "2938dbf24241ec348f87895fba6a4b24",
+    "url": "/counting/static/media/potato.2938dbf2.svg"
+  },
+  {
+    "revision": "71c4708a73d0ec24310c5727921df34a",
+    "url": "/counting/static/media/pumpkin.71c4708a.svg"
+  },
+  {
+    "revision": "b486f3c6e314afae2122cf5e04bc5621",
+    "url": "/counting/static/media/puzzle-game.b486f3c6.svg"
+  },
+  {
+    "revision": "b92ddd1542c0dc5f2ccbfc30f7cc2113",
+    "url": "/counting/static/media/radish.b92ddd15.svg"
+  },
+  {
+    "revision": "956dc6e4fb30261d254d5d556ef4d87d",
+    "url": "/counting/static/media/rain.956dc6e4.svg"
+  },
+  {
+    "revision": "f7955deb26d94a2da2f021ae405afdfd",
+    "url": "/counting/static/media/rainbow.f7955deb.png"
+  },
+  {
+    "revision": "c4d923b981c78230e973a65b6250da47",
+    "url": "/counting/static/media/shop-game.c4d923b9.svg"
+  },
+  {
+    "revision": "46f4edbda9d0ec1c6148baa1c5c30bd7",
+    "url": "/counting/static/media/shopping-cart.46f4edbd.svg"
+  },
+  {
+    "revision": "b7d4c294ecd6041a69a8f74f1d63aea0",
+    "url": "/counting/static/media/spider.b7d4c294.svg"
+  },
+  {
+    "revision": "9e8e902669856ae9124d5f74d3a1c186",
+    "url": "/counting/static/media/spinach.9e8e9026.svg"
+  },
+  {
+    "revision": "cf33ec77a7ca266733e9a0e9e8641db7",
+    "url": "/counting/static/media/stars.cf33ec77.svg"
+  },
+  {
+    "revision": "0f028a2b71e9b9558b58efc97dfa23b6",
+    "url": "/counting/static/media/start-button.0f028a2b.svg"
+  },
+  {
+    "revision": "b7931bae80e691c7531de66c447bb572",
+    "url": "/counting/static/media/strawberry.b7931bae.svg"
+  },
+  {
+    "revision": "eeadcd43288c92e6219f4b105e806ee5",
+    "url": "/counting/static/media/sun.eeadcd43.svg"
+  },
+  {
+    "revision": "4826181a9b05a17c486fe9c57685211f",
+    "url": "/counting/static/media/tomato.4826181a.svg"
+  },
+  {
+    "revision": "a7028a0ff21b9387c5fc0b3f92f0b481",
+    "url": "/counting/static/media/turnip.a7028a0f.svg"
+  },
+  {
+    "revision": "9f618796b8d0d29f9f4b1171fb98357c",
+    "url": "/counting/static/media/tyre3.9f618796.svg"
+  },
+  {
+    "revision": "9b004dbc24d07b07f8fe89399f7811ea",
+    "url": "/counting/static/media/watermelon.9b004dbc.svg"
+  }
+]);
